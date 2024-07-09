1137ba6fa28df40e53904815be91590dcdc043f9
unknown <m.adamfalah12@gmail.com>
2024-07-09 14:13:14 +0700

    fixing

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-import subprocess
-import sys
-
-def install_packages():
-    # Upgrade pip
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
-
-    # Install packages from requirements.txt
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
-
-try:
-    install_packages()
-except Exception as e:
-    print(f"An error occurred: {e}")
-
 #WEB library
 import streamlit.components.v1 as components
 import streamlit as st
